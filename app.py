af209f47c1a2a2db1cef2db84245a1fabd34d245
Kowsik1407 <111348287+Kowsik1407@users.noreply.github.com>
2023-04-17 18:03:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/",methods=["GET"])
 def hello():
     return Response("Hello tailwinds-1")
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=80, debug=True)
+    app.run("0.0.0.0", port=5000, debug=True)
